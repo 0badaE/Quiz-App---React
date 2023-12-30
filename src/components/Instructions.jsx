25f92f0f25dc8bd432e1ae0a517979edc043fe51
Obi.É <105763414+0badaE@users.noreply.github.com>
2023-12-29 23:20:37 -0500

    fix

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -2,7 +2,7 @@ function Instructions (props){
     return(
     <>
         <main className = "StartPage">
-        <img className = "blob1"src= "0badaE/Quiz-App---React/src/pictures/blob5.jpg>
+        <img className = "blob1"src= "0badaE/Quiz-App---React/src/pictures/blob5.jpg"/>
         <section className = "start-page-text">
             <h1> Quizzical </h1>
             <p>Quizzical asks 5 random questions on various topics. It’s a fun way to test your knowledge and learn new things.</p>
