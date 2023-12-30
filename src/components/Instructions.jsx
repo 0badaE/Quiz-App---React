f0cb51cc45b35b8c68148379e05bfac95d4b8361
Obi.É <105763414+0badaE@users.noreply.github.com>
2023-12-29 23:28:52 -0500

    d

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -2,7 +2,7 @@ function Instructions (props){
     return(
     <>
         <main className = "StartPage">
-        <img className = "blob1" src= "https://ibb.co/m5GGxQv"/>
+        <img className = "blob1" src= "https://i.ibb.co/m5GGxQv/blob-5.png"/>
         <section className = "start-page-text">
             <h1> Quizzical </h1>
             <p>Quizzical asks 5 random questions on various topics. It’s a fun way to test your knowledge and learn new things.</p>
